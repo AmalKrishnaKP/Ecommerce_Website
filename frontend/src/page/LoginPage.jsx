a36dccdd56706b10b373c7696d91340cd8ee1a4d
AmalKrishnaKP <amalkrishnakp04@gmail.com>
2025-07-25 01:41:00 +0530

    debugged

--- frontend/src/page/LoginPage.jsx
+++ frontend/src/page/LoginPage.jsx
@@ -26,7 +26,7 @@ const LoginPage = () => {
     login(formdata)  
   }
   return (
-    <div className=' flex  items-start justify-center opacity-100 bg-[url(https://res.cloudinary.com/dezj3e0et/image/upload/v1753387277/bgimage_l2bvlf.png)]  px-3  pt-30'>
+    <div className=' flex  items-start justify-center  opacity-100 bg-[url(https://res.cloudinary.com/dezj3e0et/image/upload/v1753387277/bgimage_l2bvlf.png)]  px-3  pt-30'>
         
         <LoginForm />
         
--- frontend/src/page/SignupPage.jsx
+++ frontend/src/page/SignupPage.jsx
@@ -5,7 +5,7 @@ const SignupPage = () => {
     // preventdefault()
     
   return (
-    <div className='flex w-full bg-[url(https://res.cloudinary.com/dezj3e0et/image/upload/v1753387277/bgimage_l2bvlf.png)] items-center justify-center opacity-100  px-3  pt-20  '>
+    <div className=' flex  items-start justify-center  opacity-100 bg-[url(https://res.cloudinary.com/dezj3e0et/image/upload/v1753387277/bgimage_l2bvlf.png)]  px-3  pt-30'>
       
         <SignupForm />
     </div>
